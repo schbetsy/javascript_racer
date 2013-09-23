19950b71a1852c1a476e6db56dd5542bf62c69ec
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-23 17:42:25 -0500

    made login page

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,16 @@
+#== GET ====================
 get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+
+
+#== POST ====================
+
+post '/startgame' do
+  @p1 = Player.find_or_create_by_initials(params[:p1][:initials])
+  @p2 = Player.find_or_create_by_initials(params[:p2][:initials])
+  erb :gameplay
+end
+
