429db579d887ada1550201fbb1111e9859edcef8
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-23 16:02:35 -0500

    players can race across screen, get message when one finishes

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,31 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+    $('#player1_strip td:nth-child(2)').attr('class','active')
+    $('#player2_strip td:nth-child(2)').attr('class','active')
+    var player_1_position = 2
+    var player_2_position = 2
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $(document).on('keyup', function(event) {
+    //Q = 81
+    //P = 80
+    if (event.keyCode == 81) {
+        p1_space_selector = 'td:nth-child('+player_1_position+')'
+        $("#player1_strip td").removeClass('active')
+        player_1_position += 1
+        $("#player1_strip").find(p1_space_selector).attr('class','active')
+        if (player_1_position > track_length+1) {
+            alert("player 1 won!")
+        }
+    } else if (event.keyCode == 80) {
+        p2_space_selector = 'td:nth-child('+player_2_position+')'
+        $("#player2_strip td").removeClass('active')
+        player_2_position += 1
+        $("#player2_strip").find(p2_space_selector).attr('class','active')
+         if (player_2_position > track_length+1) {
+            alert("player 2 won!")
+            
+        }
+    }
+
+
+  });
 });
