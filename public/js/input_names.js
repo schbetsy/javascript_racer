69d4da28d36b941b5d9ace03324047d51a73ba15
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-24 20:50:38 -0500

    use intro page js to limit to 6 players

--- public/js/input_names.js
+++ public/js/input_names.js
@@ -1,6 +1,9 @@
 function add_player() {
     var last_space = $('form input').last().data('id');
     var next_space = last_space + 1;
+    if (next_space === 6) {
+      $('.add-player').remove();
+    }
     var next_input = "<h4>vs.</h4><input type='text' name="+next_space+" data-id="+next_space+" placeholder='Player "+next_space+"'></input>";
     $('[data-id='+last_space+']').first().after(next_input);
 }
