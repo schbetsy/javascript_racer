06712a81a645080bfb8b16470ff0331c7936abb3
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-24 20:12:06 -0500

    let user add login spaces on homepage

---
+++ public/js/input_names.js
@@ -0,0 +1,11 @@
+function add_player() {
+    var last_space = $('form input').last().data('id');
+    var next_space = last_space + 1;
+    var next_input = "<h4>vs.</h4><input type='text' name="+next_space+" data-id="+next_space+" placeholder='Player "+next_space+"'></input>";
+    $('[data-id='+last_space+']').first().after(next_input);
+}
+
+$(document).ready(function() {
+    $('.add-player').on('click', add_player);
+
+});
